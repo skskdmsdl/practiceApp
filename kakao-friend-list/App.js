16aa27d4812fe2b46be1989de613e160348820f7
skskdmsdl <skskdmsdl@gmail.com>
2023-01-21 23:09:36 +0900

    header 그리기

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Header from "./src/Header";
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+
+const statusBarHeight = getStatusBarHeight(true);
+const bottomSpace = getBottomSpace();
+
+console.log(`${Platform.OS}: ${statusBarHeight}, ${bottomSpace}`);
+
 
 export default function App() {
   return (
+    // SafeAreaView는 상단바 영역 이후부터 View가 그려짐 (하단 탭바때문에 사용 안할 예정)
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
@@ -13,8 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: statusBarHeight,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
