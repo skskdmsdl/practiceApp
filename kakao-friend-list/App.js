a376b53b21c7cf1928fec1860920860687012577
skskdmsdl <skskdmsdl@gmail.com>
2023-01-22 20:51:41 +0900

    myprofile 추가 및 margin 컴포넌트 추가

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from "./src/Header";
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import { friendProfiles, myProfile } from "./src/data";
+import Header from "./src/Header";
+import MyProfile from './src/MyProfile';
+import Margin from "./src/Margin";
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
@@ -12,10 +16,19 @@ console.log(`${Platform.OS}: ${statusBarHeight}, ${bottomSpace}`);
 
 export default function App() {
   return (
-    // SafeAreaView는 상단바 영역 이후부터 View가 그려짐 (하단 탭바때문에 사용 안할 예정)
+    // SafeAreaView는 상단바 영역 이후부터 View가 그려짐 (하단 탭바때문에 사용 안할 예정-react-native 라이브러리)
     <SafeAreaProvider >
     <SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
       <Header />
+
+      <Margin height={10} />
+
+      <MyProfile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+      />
+
     </SafeAreaView>
     </SafeAreaProvider>
     
